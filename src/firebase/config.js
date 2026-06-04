89e68f99125492ea86d819506345d7fae9a840db
Nimesh-Dilushan <dilushannimesh99@gmail.com>
2026-06-05 02:21:34 +0530

    succesfully replaced harcoded firebase credentials with env variables.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; //Firebase Authentication
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// This object tells Vite to pull your keys dynamically from the hidden .env file
 const firebaseConfig = {
-    apiKey: "AIzaSyD6Kmf-7I0wysaL-DPbEyrcJyWtErT1eCU",
-    authDomain: "webassignment-11507.firebaseapp.com",
-    projectId: "webassignment-11507",
-    storageBucket: "webassignment-11507.firebasestorage.app",
-    messagingSenderId: "402893173351",
-    appId: "1:402893173351:web:980af323d197a12e011ac2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase app and Firestore database
+// Initialize Firebase services for your application modules
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
